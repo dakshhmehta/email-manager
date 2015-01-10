bcada4f021399d7bc52e1731a2a8524b6e27a33d
Daksh Mehta <daksh@dristal.com>
2015-01-10 17:22:11 +0530

    Refreshed index

---
+++ src/Dakshhmehta/EmailManager/EmailManagerServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Dakshhmehta\EmailManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class EmailManagerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('dakshhmehta/email-manager');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
